ec1cee87439ff0a53cd1d80647b18a706ff9fb4d
joelrocero <joelrocero19@gmail.com>
2021-10-28 23:56:43 +0800

    Add Validation

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -17,11 +17,10 @@ namespace API.Helpers
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
                 .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
                 .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+            CreateMap<Address, AddressDto>();
+            CreateMap<CustomerBasketDto, CustomerBasket>();
             CreateMap<BasketItemDto, BasketItem>().ReverseMap();
 
-            CreateMap<Address, AddressDto>();
-
-            CreateMap<CustomerBasketDto, CustomerBasket>();
         }
     }
 }
