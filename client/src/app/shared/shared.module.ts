967fc8a31474f502d72e36ae149cb41a6abfdb83
joelrocero <joelrocero19@gmail.com>
2021-10-30 22:38:31 +0800

    End of Section 18

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -5,15 +5,24 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { PagerComponent } from './components/pager/pager.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 @NgModule({
-  declarations: [PagerComponent, PagingHeaderComponent, OrderTotalsComponent],
+  declarations: [
+    PagerComponent,
+    PagingHeaderComponent,
+    OrderTotalsComponent,
+    TextInputComponent,
+  ],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
     CarouselModule.forRoot(),
+    BsDropdownModule.forRoot(),
     ReactiveFormsModule,
     FormsModule,
   ],
@@ -21,9 +30,11 @@ import { OrderTotalsComponent } from './components/order-totals/order-totals.com
     PaginationModule,
     PagingHeaderComponent,
     OrderTotalsComponent,
+    TextInputComponent,
     CarouselModule,
     PagerComponent,
     ReactiveFormsModule,
+    BsDropdownModule,
     FormsModule,
   ],
 })
